f6d1ee466a63da439df9c07b0d9416869857b749
Kevin Schroeder <schroederkevin94@gmail.com>
2025-03-10 14:47:16 -0500

    Prevent Calendar Duplicates

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,7 +1,8 @@
 // Function to create calendar heatmap
 function createCalendarHeatmap(data) {
 
-  d3.select("calendar-container").remove();
+  // Remove the previously rendered calendar SVG if it exists
+  d3.select("#calendar svg").remove();
 
   // Validate and clean date entries
   const validData = data.filter(d => {
